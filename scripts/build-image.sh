31505e316d0ebbe6821472493d4b5015ce7c7c60
Cameron <blog@cameron.technology>
2024-09-09 17:30:15 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,8 +1,6 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$NAME"
-: "$DIRECTORY"
 
 docker build -t $CONTAINER_REGISTRY/book_catalog:$VERSION --file ./book_catalog/Dockerfile ./book_catalog
 docker build -t $CONTAINER_REGISTRY/inventory_management:$VERSION --file ./inventory_management/Dockerfile ./inventory_management
