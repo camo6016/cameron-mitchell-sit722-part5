43cd21276b096a2d782146a1063bdc0e304087bd
cameron <cameron@localhost.localdomain>
2024-09-09 17:21:14 +1000

    Build images

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,1 +1,9 @@
-# Your Solution
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+: "$NAME"
+: "$DIRECTORY"
+
+docker build -t $CONTAINER_REGISTRY/book_catalog:$VERSION --file ./book_catalog/Dockerfile ./book_catalog
+docker build -t $CONTAINER_REGISTRY/inventory_management:$VERSION --file ./inventory_management/Dockerfile ./inventory_management
+
