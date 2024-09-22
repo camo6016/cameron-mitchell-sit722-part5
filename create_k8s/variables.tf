68f494544a7044f366e7315e17329247d6f6da1a
cameron <cameron@localhost.localdomain>
2024-09-22 21:09:03 +1000

    Added terraform

---
+++ create_k8s/variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "ninetwodistinction"
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable kubernetes_version {
+  default = "1.30.3"
+}
