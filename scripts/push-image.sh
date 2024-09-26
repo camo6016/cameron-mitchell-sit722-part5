c1fc7722daf327090d888f548686024e0e270917
Cameron <blog@cameron.technology>
2024-09-26 16:42:57 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -2,6 +2,6 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-az acr login $CONTAINER_REGISTRY
+az acr login -n $CONTAINER_REGISTRY
 docker push $CONTAINER_REGISTRY/book_catalog:$VERSION 
 docker push $CONTAINER_REGISTRY/inventory_management:$VERSION 
