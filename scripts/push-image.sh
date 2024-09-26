b436225db11b16dceb33c9e43fe05b3e75ab07b0
Cameron <blog@cameron.technology>
2024-09-26 16:40:00 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,9 +1,7 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$REGISTRY_PW"
-: "$REGISTRY_UN"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+az acr login $CONTAINER_REGISTRY
 docker push $CONTAINER_REGISTRY/book_catalog:$VERSION 
 docker push $CONTAINER_REGISTRY/inventory_management:$VERSION 
